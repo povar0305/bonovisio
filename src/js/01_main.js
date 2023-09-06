05f18176b83615f0447146e5cba5282307ca651b
Povarnitsina Sonya <73982948+povar0305@users.noreply.github.com>
2023-09-06 16:30:24 +0300

    Блок спикеров + адаптив

---
+++ src/js/01_main.js
@@ -0,0 +1,22 @@
+
+const swiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        0: { slidesPerView: 1 },
+        460: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1240: {
+            slidesPerView: 2,
+            spaceBetween: 42
+        }
+    }
+});
